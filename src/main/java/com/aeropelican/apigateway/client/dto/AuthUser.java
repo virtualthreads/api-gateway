528c6a02cae0e7b4761aabffcea0fa9b9e8aa7fa
Virtual Thread <rama.krishna@aeropelicantech.com>
2026-08-29 17:00:55 +0530

    Update gateway authentication DTO

--- src/main/java/com/aeropelican/apigateway/client/dto/AuthUser.java
+++ src/main/java/com/aeropelican/apigateway/client/dto/AuthUser.java
@@ -2,11 +2,15 @@ package com.aeropelican.apigateway.client.dto;
 
 import lombok.Builder;
 
+import java.util.UUID;
+
 @Builder
 public record AuthUser(
+        UUID userId,
         String firstname,
         String lastname,
         String email,
-        String hashedPassword
+        String hashedPassword,
+        String role
 ) {
 }
