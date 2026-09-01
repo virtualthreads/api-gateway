04498fb42e797b0d595afa2f43ed6e8d3423d56a
Sowmya TG <soumya111tg@gmail.com>
2026-09-01 21:59:41 +0530

    Implemented RBAC mapping with security configuration

--- src/main/java/com/aeropelican/apigateway/service/CustomUserDetailsService.java
+++ src/main/java/com/aeropelican/apigateway/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.aeropelican.apigateway.service;
 import com.aeropelican.commonsservice.user.clients.UserServiceClient;
 import com.aeropelican.commonsservice.user.dto.response.ApiResponse;
 import com.aeropelican.commonsservice.user.dto.response.UserAuthResponse;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
@@ -22,14 +23,20 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
-        log.info("Attempting to fetch user details for email: {}", email);
-        ApiResponse<UserAuthResponse> response = userServiceClient.getUserByEmailForAuth(email);
-        UserAuthResponse userAuthResponse = response.getData();
+        try {
+            ApiResponse<UserAuthResponse> response = userServiceClient.getUserByEmailForAuth(email);
+            if (response == null || response.getData() == null) {
+                throw new UsernameNotFoundException("User not found: " + email);
+            }
 
-        return User
-                .withUsername(userAuthResponse.email())
-                .password(userAuthResponse.passwordHash())
-                .authorities("ROLE_ADMIN")
-                .build();
+            UserAuthResponse user = response.getData();
+            return User.withUsername(user.email())
+                    .password(user.passwordHash())
+                    .authorities("ROLE_ADMIN")
+                    .build();
+
+        } catch (FeignException.Unauthorized | FeignException.NotFound e) {
+            throw new UsernameNotFoundException("User not found: " + email, e);
+        }
     }
 }
